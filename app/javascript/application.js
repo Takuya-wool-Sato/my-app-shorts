9d7e721a4f154786e309a7641fe518e105bf0d7d
Takuya-wool-Sato <takuya@hab-co.jp>
2024-04-22 01:35:12 +0900

    daisyUl,Splideオプション追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import Splide from '@splidejs/splide';
+
+var splide = new Splide( '.splide', {
+  type    : 'loop',
+  autoplay: true,
+  arrows: false, // 矢印ボタンを非表示
+  interval: 6000
+});
+
+splide.mount();
