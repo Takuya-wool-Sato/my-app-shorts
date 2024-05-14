b9382588e6252f84e8f15b5d84b2051d4a89ab7c
Takuya-wool-Sato <takuya@hab-co.jp>
2024-05-15 01:40:03 +0900

    fix

--- db/migrate/20240514163423_add_user_id_categories.rb
+++ db/migrate/20240514163423_add_user_id_categories.rb
@@ -1,5 +1,5 @@
 class AddUserIdCategories < ActiveRecord::Migration[7.0]
   def change
-    add_reference :categoriess, :user, null: false, foreign_key: true
+    add_reference :categories, :user, null: false, foreign_key: true
   end
 end
