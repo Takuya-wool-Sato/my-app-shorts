203180ea16c42af5c54db56ecde590a243629b7a
Takuya-wool-Sato <takuya@hab-co.jp>
2024-12-11 12:11:43 +0900

    リクエスト文の変更

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,7 +3,7 @@ class ChatController < ApplicationController
     if params[:question].present?
       service = ChatGptService.new
       begin
-        @response = JSON.parse(service.chat("以下のお題に対して、有益な情報をタイトルとその具体例をJSON配列（キーは数字でお願いします）で5グループください 具体例は〜が良いや、〜できるみたいな形で終わらせてください。お題：#{params[:question]}"))
+        @response = JSON.parse(service.chat("以下のお題に対して、有益な情報をタイトル(キー:title)とその具体例(キー:example)をJSON配列（キーは数字でお願いします）で5グループください 具体例は〜が良い、〜できるみたいな形で終わらせてください。お題：#{params[:question]}"))
         # @responseをeachで回してpostsテーブルに保存
         @response.each do |key, value|
           if value.key?("title") && value.key?("example")
@@ -17,11 +17,11 @@ class ChatController < ApplicationController
             Rails.logger.error("Missing keys in response: #{value}")
           end
         end
+        # render json: { response: @response }
       rescue JSON::ParserError => e
         @response = { error: "エラーが発生しました: #{e.message}" }
       end
     end
-    # render json: { response: @response }
     @categories = Category.where(user_id: current_user.id)
     @posts = current_user.posts.order("RAND()").all
     @post = Post.new
